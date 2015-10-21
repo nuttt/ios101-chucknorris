8c5a99ce85daca36fdc04558c54c0ac3dfbde81b
Nuttapon Pattanavijit <nuttt@oozou.com>
2015-10-22 00:55:07 +0700

    Initial Commit

---
+++ ios101-chucknorris/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios101-chucknorris
+//
+//  Created by Nut on 10/22/15.
+//  Copyright © 2015 nuttt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios101-chucknorris/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios101-chucknorris
+//
+//  Created by Nut on 10/22/15.
+//  Copyright © 2015 nuttt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
